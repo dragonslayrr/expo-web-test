f485f4d22b3816fc0db8cfd310537f04fe3e37ca
dragonslayrr <connerszak@gmail.com>
2024-02-27 13:34:31 -0500

    initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { Text, View, StyleSheet, SafeAreaView, Pressable} from 'react-native'
+import React from "react"
+
+import Test from "./components/Test"
+
+export default function App() {
+    return (
+        <>
+            <View>
+                <Test />
+            </View>
+        </>
+    )
+}
---
+++ components/Test.tsx
@@ -0,0 +1,8 @@
+import { Text, View, StyleSheet, SafeAreaView, Pressable} from 'react-native'
+import React from "react"
+
+export default function Test() {
+    return (
+        <Text>Test PLS WORK</Text>
+    )
+}
