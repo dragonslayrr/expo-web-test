6dd5b8d03c86011da57ba72d39eb424e4b3d4bb6
dragonslayrr <connerszak@gmail.com>
2024-02-27 13:55:42 -0500

    styles

--- App.tsx
+++ App.tsx
@@ -7,8 +7,28 @@ export default function App() {
     return (
         <>
             <SafeAreaView>
-                <Test />
+                <View style={styles.container}>
+                    <Test style={styles.test} />
+                </View>
             </SafeAreaView>
         </>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "flex-start",
+        paddingTop: 25,
+        backgroundColor: "#0b1a25"
+    },
+    test: {
+        fontSize: 25,
+        color: "white",
+    }
+})
+
+
+
+// https://github.com/dragonslayrr/expo-web-test.git
